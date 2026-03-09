da1b6d612780ee1c864d624e10f81ee43e36c6c2
Pedro Paulo Portella <10197081+pedropauloportella@users.noreply.github.com>
2026-03-09 18:53:25 -0300

    Add password creation for bcrypt hash generation

--- scripts/seed-user.js
+++ scripts/seed-user.js
@@ -1,4 +1,5 @@
 // Simple script to print bcrypt hash for admin password
+//create your password
 const bcrypt = require('bcryptjs');
 const pw = process.argv[2] || 'changeme';
 const hash = bcrypt.hashSync(pw, 10);
